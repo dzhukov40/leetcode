a0efe7f866f8e51cfb32f54f5e649388f71bfe50
dzhukov40 <dzhukov40.v@mail.ru>
2019-05-16 19:44:55 +0300

    решили задачку https://leetcode.com/problems/valid-parentheses/submissions/

---
+++ src/main/java/ru/doneathome/leetcode/ValidParentheses.java
@@ -0,0 +1,70 @@
+package ru.doneathome.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    /**
+     * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     *
+     * An input string is valid if:
+     *
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Note that an empty string is also considered valid.
+     *
+     * Example 1:
+     *
+     * Input: "()"
+     * Output: true
+     * Example 2:
+     *
+     * Input: "()[]{}"
+     * Output: true
+     * Example 3:
+     *
+     * Input: "(]"
+     * Output: false
+     * Example 4:
+     *
+     * Input: "([)]"
+     * Output: false
+     * Example 5:
+     *
+     * Input: "{[]}"
+     * Output: true
+     */
+    public boolean isValid(String s) {
+
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+
+
+        Stack<Character> stack = new Stack<>();
+
+
+        char[] chars = s.toCharArray();
+
+
+        for(int i = 0; i < chars.length; i++) {
+
+            if(chars[i] == '(' || chars[i] == '{' || chars[i] == '[') {
+                stack.push(chars[i]);
+            } else {
+
+                if( stack.empty() || !map.get(stack.peek()).equals(chars[i]) ) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+
+
+}
